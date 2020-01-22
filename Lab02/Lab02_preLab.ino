b378be00b0330ae826f4120fa53b8349045bdb44
HappyfaceJack <48802673+HappyfaceJack@users.noreply.github.com>
2020-01-22 15:06:32 -0500

    Update Lab02_preLab.ino

--- Lab02/Lab02_preLab.ino
+++ Lab02/Lab02_preLab.ino
@@ -1,38 +1,71 @@
-/**
-The purpose of the code is to have a blinking LED with specific sequence seen in the provided manual.
-*/
-
 unsigned long past = 0;
-const long delayMs = 100;
-int pin[4] = {4, 5, 6, 7}; //The pin numbers maybe changed to suitable numbers according to the wiring
-
+long delayMs = 100;
+int pin[4] = {4, 5, 6, 7};
+int state=1;
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(pin[i], OUTPUT);
   }
+ pinMode(A0,INPUT);
 }
 
 void loop() {
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pin[i], HIGH);
-    wait();
+ // delayMs =analogRead(A0);
+  if(state%2!=0){
+  delayMs=300;
+  } else {
+  delayMs=100;
   }
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pin[i], LOW);
-    wait();
-  }
-  for (int i = 3; i >= 0; i--) {
-    digitalWrite(pin[i], HIGH);
-    wait();
-  }
-  for (int i = 3; i >= 0; i--) {
-    digitalWrite(pin[i], LOW);
-    wait();
+  if (millis() - past >= delayMs) {
+    past = millis();
+    if (state == 16) {
+      state = 1;
+    }
+    switch (state) {
+      case 1:
+        writeAll(1, 0, 0, 0);
+        break;
+      case 2:
+        writeAll(1, 1, 0, 0);
+        break;
+      case 3:
+        writeAll(0, 1, 0, 0);
+        break;
+      case 4:
+        writeAll(0, 1, 1, 0);
+        break;
+      case 5:
+        writeAll(0, 0, 1, 0);
+        break;
+      case 6:
+        writeAll(0, 0, 1, 1);
+        break;
+      case 7:
+        writeAll(0, 0, 0, 1);
+        break;
+      case 8:
+        writeAll(0, 0, 1, 1);
+        break;
+      case 9:
+        writeAll(0, 0, 1, 0);
+        break;
+      case 10:
+        writeAll(0, 1, 1, 0);
+        break;
+      case 11:
+        writeAll(0, 1, 0, 0);
+        break;
+      case 12:
+        writeAll(1, 1, 0, 0);
+        break;
+    }
+    state++;
   }
 }
 
-void wait() {//a substitute to the delay(), where it is a constraint in the lab
-  while (millis() < past + delayMs) {
-  }
-  past = millis();
+void writeAll(int i, int j, int k, int l) {
+  digitalWrite(4, i);
+  digitalWrite(5, j);
+  digitalWrite(6, k);
+  digitalWrite(7, l);
 }
