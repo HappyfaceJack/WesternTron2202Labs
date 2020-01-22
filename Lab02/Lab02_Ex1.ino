01029ab7fa25a0139a9db4b650dcbeddc66d7eb7
HappyfaceJack <48802673+HappyfaceJack@users.noreply.github.com>
2020-01-22 15:02:25 -0500

    Non-blocking Delay
    Used an if statement to run the code instead of delay()/ blocking code to wait in a certain loop

--- Lab02/Lab02_Ex1.ino
+++ Lab02/Lab02_Ex1.ino
@@ -1,37 +1,77 @@
-/**
-The purpose of the code is to have a blinking LED with specific sequence seen in the provided manual.
-*/
 unsigned long past = 0;
-const long delayMs = 300; //a delay which maybe changed to the user's content
-int pin[4] = {4, 5, 6, 7}; //pin number maybe changed
-
+long delayMs = 100;
+int pin[4] = {4, 5, 6, 7};
+int state=1;
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(pin[i], OUTPUT);
   }
+ pinMode(A0,INPUT);
 }
 
 void loop() {
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pin[i], HIGH);
-    wait();
-    digitalWrite(pin[i+1], HIGH);
-    wait();
-    digitalWrite(pin[i], LOW);
-    wait();
-  }
-  for (int i = 3; i >= 0; i--) {
-   digitalWrite(pin[i], HIGH);
-    wait();
-    digitalWrite(pin[i-1], HIGH);
-    wait();
-    digitalWrite(pin[i], LOW);
-    wait();
+  if (millis() - past >= delayMs) {
+    past = millis();
+    if (state == 16) {
+      state = 1;
+    }
+    switch (state) {
+      case 1:
+        writeAll(0, 0, 0, 0);
+        break;
+      case 2:
+        writeAll(1, 0, 0, 0);
+        break;
+      case 3:
+        writeAll(1, 1, 0, 0);
+        break;
+      case 4:
+        writeAll(1, 1, 1, 0);
+        break;
+      case 5:
+        writeAll(1, 1, 1, 1);
+        break;
+      case 6:
+        writeAll(0, 1, 1, 1);
+        break;
+      case 7:
+        writeAll(0, 0, 1, 1);
+        break;
+      case 8:
+        writeAll(0, 0, 0, 1);
+        break;
+      case 9:
+        writeAll(0, 0, 0, 0);
+        break;
+      case 10:
+        writeAll(0, 0, 0, 1);
+        break;
+      case 11:
+        writeAll(0, 0, 1, 1);
+        break;
+      case 12:
+        writeAll(0, 1, 1, 1);
+        break;
+      case 13:
+        writeAll(1, 1, 1, 1);
+        break;
+      case 14:
+        writeAll(1, 1, 1, 0);
+        break;
+      case 15:
+        writeAll(1, 1, 0, 0);
+        break;
+      case 16:
+        writeAll(1, 0, 0, 0);
+        break;
+    }
+    state++;
   }
 }
 
-void wait() {
-  while (millis() < past + delayMs) {
-  }
-  past = millis();
+void writeAll(int i, int j, int k, int l) {
+  digitalWrite(4, i);
+  digitalWrite(5, j);
+  digitalWrite(6, k);
+  digitalWrite(7, l);
 }
