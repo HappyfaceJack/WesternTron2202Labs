a3858d2be481d979d5fb4ec3e1d0eeacd577b865
HappyfaceJack <48802673+HappyfaceJack@users.noreply.github.com>
2020-01-22 15:07:34 -0500

    Non-blocking Delay

--- Lab02/Lab02_Ex4.ino
+++ Lab02/Lab02_Ex4.ino
@@ -1,65 +1,201 @@
-/**
-The circuit relies on both button and potentiometer to alter the delay.
-*/
-unsigned long past = 0, initial = 0, released = 0, delayMs = 100;
-int pin[4] = {4, 5, 6, 7};
+#include <Servo.h>   
+
+Servo servo_test;      
+int angle = 0;
+const int potentiometer = A0;
+const int button = 2;
+const int switcher = 3;
+
+void updateState();
+long updateCounter();
+
+
+  int switchstate = 0;
+  int buttonState = 0;
+  int lastButtonState = 0; 
+  long startPressed = 0;    
+  long endPressed = 0;      
+  int timeHold = 0;        
+  int timeReleased = 0;    
+  int previousinterval = 0;
+
+unsigned long previousMillis = 0; 
+long interval = 100;       
+int state = 0;
+
 
 void setup() {
-  for (int i = 0; i < 4; i++) {
-    pinMode(pin[i], OUTPUT);
+  for(int i=4;i<=7;i++){
+  pinMode(i, OUTPUT);
   }
-  pinMode(A0, INPUT);
-  for (int i = 2; i <= 3; i++) {
-    pinMode(i, INPUT);
-  }
-  Serial.begin(13000);
+  pinMode(button, INPUT);
+  pinMode(switcher, INPUT);
+  servo_test.attach(9);   
+  Serial.begin(9600);
 }
 
 void loop() {
+   buttonState = digitalRead(button);
+  switchstate = digitalRead(switcher);
 
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pin[i], HIGH);
-    wait();
-    digitalWrite(pin[i + 1], HIGH);
-    wait();
-    digitalWrite(pin[i], LOW);
-    wait();
-  }
-  for (int i = 3; i >= 0; i--) {
-    digitalWrite(pin[i], HIGH);
-    wait();
-    digitalWrite(pin[i - 1], HIGH);
-    wait();
-    digitalWrite(pin[i], LOW);
-    wait();
+  unsigned long currentMillis = millis();
+  
+  if (switchstate == 0){
+  
+
+  interval = analogRead(potentiometer);
+  
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    state ++;
+
+    switch(state){ 
+     case 1:
+      writeAll(1,0,0,0);
+      break;
+     
+     case 2:
+      writeAll(1,1,0,0);
+      break;
+
+     case 3:
+      writeAll(0,1,0,0);
+      break;
+      
+     case 4:
+      writeAll(0,1,1,0);
+      break;
+
+     case 5:
+      writeAll(0,0,1,0);
+      break;
+    
+     case 6:
+      writeAll(0,0,1,1);
+      break;
+    
+    case 7:
+      writeAll(0,0,0,1);
+      break;
+
+    case 8:
+      writeAll(0,0,1,1);
+      break;
+
+    case 9:
+      writeAll(0,0,1,0);
+      break;
+
+    case 10:
+      writeAll(0,1,1,0);
+      break;
+
+    case 11:
+      writeAll(0,1,0,0);
+      break;
+
+     default:
+      writeAll(1,1,0,0);
+      state = 0;
+      break;
+    } 
+  }   
+ }else{
+    
+    if (buttonState != lastButtonState) { 
+     timeHold =0;
+     updateState(); 
+    } else if (previousinterval != state) {
+     interval = updateCounter();
+    }
+    lastButtonState = buttonState;
+
+    previousinterval = state;
+    
+    if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    state ++;
+
+    switch(state){ 
+     case 1:
+      writeAll(1,0,0,0);
+      break;
+     
+     case 2:
+      writeAll(1,1,0,0);
+      break;
+
+     case 3:
+      writeAll(0,1,0,0);
+      break;
+      
+     case 4:
+      writeAll(0,1,1,0);
+      break;
+
+     case 5:
+      writeAll(0,0,1,0);
+      break;
+    
+     case 6:
+      writeAll(0,0,1,1);
+      break;
+    
+    case 7:
+      writeAll(0,0,0,1);
+      break;
+
+    case 8:
+      writeAll(0,0,1,1);
+      break;
+
+    case 9:
+      writeAll(0,0,1,0);
+      break;
+
+    case 10:
+      writeAll(0,1,1,0);
+      break;
+
+    case 11:
+      writeAll(0,1,0,0);
+      break;
+
+     default:
+      writeAll(1,1,0,0);
+      state = 0;
+      break;
+    }
+ }
+}
+}
+
+void updateState() {
+  if (buttonState == 1) {
+      startPressed = millis();
+      timeReleased = startPressed - endPressed;
+
+  } else {
+      endPressed = millis();
+      timeHold = endPressed - startPressed;
+
   }
 }
-void wait() {
-
-  if (digitalRead(3) == LOW) {
-    delayMs = analogRead(A0);
-    
-    //Serial.println(delayMs); Showing the delay on Serial
-    while (millis() < past + delayMs) {
-    }
-    
-    past = millis();
-    
+long updateCounter() {
+  if (buttonState == 1) {
+      timeHold = millis() - startPressed;
+      
+  return timeHold;
   } else {
-    if (digitalRead(2) == LOW) {
-      initial = millis();
-
-    } else {
-      released = millis();
-    }
-    if (released > initial) {
-      delayMs = released - initial;
-    }
-    
-    //Serial.println(delayMs); Showing the delay on Serial
-    while (millis() < past + delayMs) {
-    }
-    
-    past = initial;
+      timeReleased = millis() - endPressed;
+      return timeHold;
   }
+}
+void writeAll(int i, int j, int k, int l) {
+  digitalWrite(4, i);
+  digitalWrite(5, j);
+  digitalWrite(6, k);
+  digitalWrite(7, l);
 }
