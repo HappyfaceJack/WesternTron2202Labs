420e07e88b1ba1b1fab4f7849acd19ba4e700977
HappyfaceJack <48802673+HappyfaceJack@users.noreply.github.com>
2020-01-22 15:03:54 -0500

    Non-blocking Delay
    Utilizing non blocking methods

--- Lab02/Lab2_Ex2.ino
+++ Lab02/Lab2_Ex2.ino
@@ -1,44 +1,72 @@
-/**
- * Utilizing a potentiometer to control the delay of the LEDs
- */
 unsigned long past = 0;
+long delayMs = 100,vdelay=0;
 int pin[4] = {4, 5, 6, 7};
-
+int state=1;
+int size=sizeof(pin);
 void setup() {
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < size; i++) {
     pinMode(pin[i], OUTPUT);
   }
-  //LED pin number may differ
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
+ pinMode(A0,INPUT);
 }
 
 void loop() {
-
-  for (int i = 0; i < 4; i++) {
-    digitalWrite(pin[i], HIGH);
-    wait();
-    digitalWrite(pin[i + 1], HIGH);
-    wait();
-    digitalWrite(pin[i], LOW);
-    wait();
+  delayMs =analogRead(A0);
+  if(state%2!=0){
+  delayMs*=3;
+  } else {
+  delayMs*=1;
   }
-  
-  for (int i = 3; i >= 0; i--) {
-    digitalWrite(pin[i], HIGH);
-    wait();
-    digitalWrite(pin[i - 1], HIGH);
-    wait();
-    digitalWrite(pin[i], LOW);
-    wait();
+  if (millis() - past >= delayMs) {
+    past = millis();
+    if (state == 16) {
+      state = 1;
+    }
+    switch (state) {
+      case 1:
+        writeAll(1, 0, 0, 0);
+        break;
+      case 2:
+        writeAll(1, 1, 0, 0);
+        break;
+      case 3:
+        writeAll(0, 1, 0, 0);
+        break;
+      case 4:
+        writeAll(0, 1, 1, 0);
+        break;
+      case 5:
+        writeAll(0, 0, 1, 0);
+        break;
+      case 6:
+        writeAll(0, 0, 1, 1);
+        break;
+      case 7:
+        writeAll(0, 0, 0, 1);
+        break;
+      case 8:
+        writeAll(0, 0, 1, 1);
+        break;
+      case 9:
+        writeAll(0, 0, 1, 0);
+        break;
+      case 10:
+        writeAll(0, 1, 1, 0);
+        break;
+      case 11:
+        writeAll(0, 1, 0, 0);
+        break;
+      case 12:
+        writeAll(1, 1, 0, 0);
+        break;
+    }
+    state++;
   }
 }
 
-void wait() {
-  //reading the potentiometer
-  long delayMs = analogRead(A0);
-  Serial.println(delayMs);
-  while (millis() < past + delayMs) {
-  }
-  past = millis();
+void writeAll(int i, int j, int k, int l) {
+  digitalWrite(4, i);
+  digitalWrite(5, j);
+  digitalWrite(6, k);
+  digitalWrite(7, l);
 }
